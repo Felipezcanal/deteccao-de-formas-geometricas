5f18609881ff43254bc855b321101f6751a47e05
Felipe Zago Canal <felipezagocanal@cliente-150-162-207-62.wireless.ufsc.br>
2019-05-28 11:51:47 -0300

    saving dataset

--- moments.py
+++ moments.py
@@ -5,15 +5,21 @@ import numpy as np
 import argparse
 import random as rng
 from pprint import pprint
+import pickle
+
 rng.seed(12345)
+
+rows = []
+cols = []
+name = []
+
+signatures = 0
+
 
 video_capture = cv.VideoCapture(0)
 def thresh_callback(val, src_gray):
     threshold = val
-
-    canny_output = cv.Canny(src_gray, threshold, threshold * 2)
-
-
+    canny_output = cv.Canny(src_gray, threshold, threshold * 3)
     contours, _ = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 
@@ -57,16 +63,73 @@ def thresh_callback(val, src_gray):
 
             # pprint(src_gray[topLeft[0]:botRight[0], topLeft[1]:botRight[1]])
             if (len(src_gray[topLeft[0]:botRight[0], topLeft[1]:botRight[1]]) > 0):
-                cv.imshow('Contours2', src_gray[topLeft[1]:botRight[1] , topLeft[0]:botRight[0]])
-                cv.waitKey(100)
+                cv.namedWindow("Contours2", cv.WINDOW_KEEPRATIO)
+                img = src_gray[topLeft[1]:botRight[1] , topLeft[0]:botRight[0]]
+                cv.imshow('Contours3', img)
+                _, img = cv.threshold(resize(img, 512, 512), 100, 255, 0)
+                cv.imshow('Contours2', img)
+                while True:
+                    key = cv.waitKey(1) & 0xFF
+                    if key == ord("p"):
+                        saveFingerPrint(img)
+                        pprint("salvo")
+                        break
+                    elif key != 255:
+                        break
 
 
 
-            print(' * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f' % (i, mu[i]['m00'], cv.contourArea(contours[i]), cv.arcLength(contours[i], True)))
+
+            # print(' * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f' % (i, mu[i]['m00'], cv.contourArea(contours[i]), cv.arcLength(contours[i], True)))
 
 
-    cv.imshow('Contours', drawing)
-    cv.waitKey(200)
+    cv.namedWindow("Contours", cv.WINDOW_KEEPRATIO)
+    cv.imshow('Contours', canny_output)
+    cv.waitKey(10)
+
+def saveFingerPrint(img):
+    h, w = img.shape
+    col = []
+    row = []
+    for i in range(0, h):
+        count = 0
+        for j in range(0, w):
+            if img[i, j] == 255:
+                count += 1
+        row.append(count)
+
+    for i in range(0, w):
+        count = 0
+        for j in range(0, h):
+            if img[i, j] == 255:
+                count += 1
+        col.append(count)
+
+    rows.append(row)
+    cols.append(col)
+    pprint("digite o nome ")
+    while True:
+        key = cv.waitKey(1) & 0xFF
+        if key != 255:
+            name.append(chr(key))
+            break
+    signatures = (rows, cols, name)
+
+    with open('signatures', 'wb') as fp:
+        pickle.dump(signatures, fp)
+
+    return
+
+
+
+def resize(image, x, y):
+    if y == 0:
+        pprint(image.shape)
+        h, w = image.shape
+        y = round(w / (h/x))
+        if(y % 2 != 0):
+            y += 1
+    return cv.resize(image, (y, x), cv.INTER_NEAREST)
 
 def readVideo():
 
@@ -81,7 +144,7 @@ def readVideo():
         src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
         src_gray = cv.blur(src_gray, (3,3))
         source_window = 'Source'
-        cv.namedWindow(source_window)
+        cv.namedWindow(source_window, cv.WINDOW_KEEPRATIO)
         cv.imshow(source_window, src)
         max_thresh = 255
         thresh = 100 # initial threshold
@@ -103,7 +166,25 @@ def readVideo():
 # cv.createTrackbar('Canny Thresh:', source_window, thresh, max_thresh, thresh_callback)
 # thresh_callback(thresh)
 
+try:
+    with open ('signatures', 'rb') as fp:
+        if fp:
+            itemlist = pickle.load(fp)
+            rows, cols, name = itemlist
+            pprint(name)
+
+except:
+    pass
+
+
 readVideo()
+
+
+
+cv.namedWindow("Contours", cv.WINDOW_KEEPRATIO)
+cv.namedWindow("Contours2", cv.WINDOW_KEEPRATIO)
+
+
 
 while True:
     key = cv.waitKey(1) & 0xFF
